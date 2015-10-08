4c75174c9fcc9c3e20ff072bba25782339caedd8
syh25 <shane.hwang@yale.edu>
2015-10-08 23:40:43 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+app.get('/nickname', function(request, response){
+    response.send('exuberant-dragonfly');
 });
 
 app.listen(process.env.PORT || 4000);
